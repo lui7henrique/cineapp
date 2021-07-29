ed43c37049fc3d2a8eccf88bdb0a8e06856cfd46
Luiz Henrique <7henrique18@gmail.com>
2021-07-28 21:02:47 -0300

    Fix to vercel deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
 import { GlobalStyles } from "../styles/Theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
