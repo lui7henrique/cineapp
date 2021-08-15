fffa560dc2e03bb7e970613ead38acd1911b9f80
Luiz Henrique <7henrique18@gmail.com>
2021-08-14 21:56:34 -0300

    feat: pwa and manifest

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const withPWA = require("next-pwa");
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    disable: !isProd,
+  },
 };
